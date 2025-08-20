7091c00a0c1fcc126149d97d9961a5069b23c493
faizakiran6357 <faizakiran571@gmail.com>
2025-08-20 14:06:33 +0500

    Added Detail screen UI

--- lib/Detail _Screen.dart
+++ lib/Detail _Screen.dart
@@ -6,15 +6,23 @@ class DetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFFFFFF),
       appBar: AppBar(
-        title:  Text("Detail Quiz"),
+        backgroundColor: Color(0xFFFFFFFF),
+        title: Text(
+          "Detail Quiz",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
         leading: IconButton(
-          icon:  Icon(Icons.arrow_back),
+          icon: Icon(Icons.arrow_back_ios),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-        actions:[
+        actions: [
           Icon(Icons.more_vert),
         ],
       ),
@@ -23,40 +31,69 @@ class DetailScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Quiz Card
             Card(
+              color:Color(0xFFFFFFFF),
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)),
+                  borderRadius: BorderRadius.circular(10)),
+            
               elevation: 3,
               child: Padding(
-                padding: const EdgeInsets.all(12),
+                padding: EdgeInsets.all(12),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Row(
                       children: [
-                         CircleAvatar(
-                          radius: 24,
-                          backgroundColor: Colors.blueAccent,
-                          child: Icon(Icons.sports_soccer, color: Colors.white),
-                        ),
-                         SizedBox(width: 12),
+                          Container(
+                                  height: 50,
+                                  width:  50,
+                               decoration: BoxDecoration(
+                              color: Color(0xFFEEF2FF),
+                             borderRadius: BorderRadius.circular(12),
+                                  ),
+                              child: Center(
+                              child: Image.asset(
+                             "assests/sport.png",
+                               height: 40,
+                               width:  40,
+                             ),
+                           ),
+                          ),
+                        SizedBox(width: 12),
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text("Sport",
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.bold)),
-                            Text("10 Question",
-                                style: TextStyle(color: Colors.grey)),
+                            Text(
+                              "Sport",
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              "10 Question",
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                                color: Color(0xFF999999),
+                              ),
+                            ),
                           ],
                         ),
                       ],
                     ),
                     Row(
                       children: [
-                        Icon(Icons.star, color: Colors.amber),
+                        Icon(Icons.star, color: Colors.yellow),
                         SizedBox(width: 4),
-                        Text("4.8"),
+                        Text(
+                          "4.8",
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
                       ],
                     ),
                   ],
@@ -65,118 +102,219 @@ class DetailScreen extends StatelessWidget {
             ),
 
             SizedBox(height: 20),
+
+            // Brief explanation
             Text(
               "Brief explanation about this quiz",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600, 
+              ),
             ),
-             SizedBox(height: 12),
+            SizedBox(height: 12),
 
+            // Card 1
             Card(
+              color:Color(0xFFFFFFFF),
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)),
+                  borderRadius: BorderRadius.circular(10)),
+            
               elevation: 2,
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: Icon(Icons.assignment, color: Colors.orange),
+            child : ListTile(
+            leading: SizedBox(
+                       height: 40,
+                       width: 40,
+                 child: Container(
+                decoration: const BoxDecoration(
+                  color: Color(0xFFFFF5E6),
+                   shape: BoxShape.circle,
+               ),
+                child: Center(
+                    child: Image.asset(
+                          "assests/assignment.png",
+                              height: 24,
+                               width: 24,
+                         ),
+                    ),
+                 ),
+              ),
+                title: Text(
+                  "10 Question",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-                title:  Text("10 Question"),
-                subtitle:  Text("10 point for a correct answer"),
+                subtitle: Text(
+                  "10 point for a correct answer",
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF999999),
+                  ),
+                ),
               ),
             ),
 
-             SizedBox(height: 12),
+            SizedBox(height: 12),
 
+            // Card 2
             Card(
+              color:Color(0xFFFFFFFF),
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)),
+                  borderRadius: BorderRadius.circular(10)),
+             
               elevation: 2,
               child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.teal,
-                  child: Icon(Icons.timer, color: Colors.white),
+              leading: SizedBox(
+                       height: 40,
+                       width: 40,
+                 child: Container(
+                decoration: const BoxDecoration(
+                   shape: BoxShape.circle,
+               ),
+                child: Center(
+                    child: Image.asset(
+                          "assests/timer.png",
+                              height: 40,
+                              width: 40,
+                         ),
+                    ),
+                 ),
+              ),
+                title: Text(
+                  "1 hour 20 min",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-                title: Text("1 hour 20 min"),
-                subtitle: Text("Total duration of the quiz"),
+                subtitle: Text(
+                  "Total duration of the quiz",
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF999999),
+                  ),
+                ),
               ),
             ),
 
-             SizedBox(height: 20),
+            SizedBox(height: 20),
 
-     // instructions text
-       Column(
-  crossAxisAlignment: CrossAxisAlignment.start,
-  children: [
-     Text(
-      "Please read the text below carefully so you can understand it",
-      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-    ),
-      SizedBox(height: 12),
+            // Instructions
+            Text(
+              "Please read the text below carefully so you can understand it",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 12),
 
-    //First point
-    Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children:[
-        Text("•  ", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        Expanded(
-          child: Text(
-            "10 point awarded for a correct answer and no marks for an incorrect answer",
-            style: TextStyle(fontSize: 13, height: 1.4),
-          ),
-        ),
-      ],
-    ),
-    SizedBox(height: 8),
+            // First point
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "•  ",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF333333),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "10 point awarded for a correct answer and no marks for an incorrect answer",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      height: 1.4,
+                      color: Color(0xFF666666),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
 
-    //Second point
-    Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children:[
-        Text("•  ", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        Expanded(
-          child: Text(
-            "Tap on options to select the correct answer",
-            style: TextStyle(fontSize: 13, height: 1.4),
-          ),
-        ),
-      ],
-    ),
-    SizedBox(height: 8),
+            // Second point
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "•  ",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF333333),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "Tap on options to select the correct answer",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      height: 1.4,
+                      color: Color(0xFF666666),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
 
-    //Third point
-    Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children:[
-        Text("•  ", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-        Expanded(
-          child: Text(
-            "Click submit if you are sure you want to complete all the quizzes",
-            style: TextStyle(fontSize: 13, height: 1.4),
-          ),
-        ),
-      ],
-    ),
-  ],
-),
-      SizedBox(height: 70),
+            // Third point
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "•  ",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF333333),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "Click submit if you are sure you want to complete all the quizzes",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      height: 1.4,
+                      color: Color(0xFF666666),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 70),
 
             // Button
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
                 onPressed: () {
-
                 },
                 style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.teal,
+                  backgroundColor: Color(0xFF21BDCA),
                   padding: EdgeInsets.symmetric(vertical: 14),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-                child:Text(
+                child: Text(
                   "Get Started",
-                  style: TextStyle(fontSize: 16, color: Colors.white),
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFFFFFFFF),
+                  ),
                 ),
               ),
             ),
